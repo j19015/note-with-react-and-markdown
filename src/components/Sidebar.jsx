c320a0fa6a81e32e39db3f780313596958e445d8
j19015 <j19015@sangi.jp>
2024-03-27 12:33:09 +0900

    add: SidebarのHTMLを記述

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 
 function Sidebar() {
   return (
-    <div className="Sidebar">
-      <h1>Sidebar</h1>
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button type="button">追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>タイトル</strong>
+            <button type="button">削除</button>
+          </div>
+          <p>ノートの内容です。</p>
+          <small className="note-meta">最終更新日: ********</small>
+        </div>
+      </div>
     </div>
   );
 }
