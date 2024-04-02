b7f61017ae58992f5efdf0383d302239ccf285ec
j19015 <j19015@sangi.jp>
2024-04-03 04:32:24 +0900

    add: リロード対応

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import uuid from 'react-uuid';
 import Sidebar from './components/Sidebar.jsx';
 import Main from './components/Main.jsx';
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [],
+  );
   const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    // ローカルストレージにnotesを保存
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   const onAddNote = () => {
     const newNote = {
