a51e64d81f480ab9109fb53c9ff3091862838455
j19015 <j19015@sangi.jp>
2024-04-02 23:59:32 +0900

    add: previewのレイアウト作成

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 
 function Main() {
   return (
-    <div className="Main">
-      <h1>Main</h1>
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="ノートの内容を記入" />
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">タイトル</h1>
+        <div className="markdown-preview">ノートの内容</div>
+      </div>
     </div>
   );
 }
